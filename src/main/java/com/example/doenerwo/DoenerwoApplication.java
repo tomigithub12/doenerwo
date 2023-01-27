b7a4df011aedb82cfa176a2b227e96c3e0abb421
tomigithub12 <gaurlalhothi@hotmail.com>
2023-01-27 19:42:07 +0100

    Initial Commit

---
+++ src/main/java/com/example/doenerwo/DoenerwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.doenerwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoenerwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoenerwoApplication.class, args);
+	}
+
+}
